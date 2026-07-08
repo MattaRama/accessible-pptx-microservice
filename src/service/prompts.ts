68421f3d64a406d287abdad28f422fdd1e5b39eb
Matthew <minerminesmc@gmail.com>
2026-07-08 09:56:18 -0400

    update AI prompts

--- src/service/prompts.ts
+++ src/service/prompts.ts
@@ -10,7 +10,7 @@ export function describeImagePrompt(
     {
       role: "system",
       content:
-        "You are an expert in digital accessibility and WCAG 2.2 guidelines. The user will provide an image. You are to describe this image in great detail, providing no added commentary- just a perfect description of the image.",
+        "You are an expert in digital accessibility and WCAG 2.2 guidelines. The user will provide an image. You are to describe this image in great detail, providing no added commentary- just a perfect description of the image. Consider how you would describe the image to someone who is blind or low-vision. Include only information relevant to the image.",
     },
     {
       role: "user",
@@ -37,7 +37,8 @@ BASE_DESCRIPTION is an AI-generated description of the image.
 CONTEXT is some possibly useful context involving the image.
 SLIDE_TEXT is all the text contained on the slide the image is from.
 You are to determine if the CONTEXT is relevant to the BASE_DESCRIPTION, and include it in the description naturally if it is relevant. If it is not relevant, don't include anything about it.
-Do not provide any additional commentary.`,
+Do not provide any additional commentary. Only include relevant information.
+When writing the updated description, consider how you would write it for someone who is blind or low-vision.`,
     },
     { role: "user", content: `BASE_DESCRIPTION:\n\n\n${baseDesc}\n\n\n` },
     { role: "user", content: `CONTEXT:\n\n\n${context.join("\n\n\n")}` },
